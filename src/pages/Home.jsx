9c429ab321250efec67b383f238b7d51b6cfcb94
Fernando Acero <ffacerot@hotmail.com>
2025-05-14 15:08:32 -0500

    Actualización de pantalla de inicio con reborde rojo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen text-center px-4">
+    <div className="flex flex-col items-center justify-center min-h-screen text-center px-4 border-4 border-red-600">
       <img src={logo} alt="Óptica Sol" className="w-60 mb-4" />
       <h2 className="text-lg text-gray-700 mb-6">Porque tus ojos merecen lo mejor</h2>
 
