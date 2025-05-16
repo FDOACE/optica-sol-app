6c8edc7d790be6548909a83c9c2a768ec96e0aef
Fernando Acero <ffacerot@hotmail.com>
2025-05-16 14:20:16 -0500

    Agrega soporte PWA e íconos

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,34 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Óptica Sol',
+        short_name: 'Óptica Sol',
+        description: 'Porque tus ojos merecen lo mejor',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: '/icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
